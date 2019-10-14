e2bb362d71bd8f33557e5399c9d10da1f1bd46ca
xhsnk <865981820>
2019-10-14 16:58:20 +0800

    新建whmTest表

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -13,6 +13,23 @@ CREATE TABLE `user` (
   `avatar_url` varchar(200) DEFAULT '' comment '头像',
   PRIMARY KEY (`id`)
 );
+
 ALTER TABLE `user` ADD UNIQUE (`username`);
 ALTER TABLE `user` ADD UNIQUE (`email`);
 ALTER TABLE `user` ADD UNIQUE (`phone`);
+
+DROP TABLE IF EXISTS `whmTest`;
+CREATE TABLE `whmTest` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`username` varchar(50) DEFAULT NULL comment '用户名',
+`password` varchar(255) DEFAULT NULL,
+`email` varchar(50) DEFAULT NULL,
+`phone` varchar(50) DEFAULT NULL,
+PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `whmTest` ADD UNIQUE (`username`);
+ALTER TABLE `whmTest` ADD UNIQUE (`email`);
+ALTER TABLE `whmTest` ADD UNIQUE (`phone`);
+
+
