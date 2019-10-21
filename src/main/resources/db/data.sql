05e7784a26e0c1efca01601e5e30cff497f429d7
WBJplum <2020675255@qq.com>
2019-10-21 17:35:29 +0800

    45

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -6,4 +6,12 @@ INSERT INTO `user` (id, username, password, role) VALUES ('5', 'user3', '1234', 
 INSERT INTO `user` (id, username, password, role) VALUES ('6', 'user4', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('7', '测试', '1234', 'user');
 
+
+INSERT INTO `wbj` (num, name, pwd, age, phone) VALUES ('01', '王彬洁', '111', '19', '13745646345');
+INSERT INTO `wbj` (num, name, pwd, age, phone) VALUES ('02', '王静鸥', '222', '20', '13741546485');
+INSERT INTO `wbj` (num, name, pwd, age, phone) VALUES ('03', '曹丽', '333', '19', '18956456345');
+INSERT INTO `wbj` (num, name, pwd, age, phone) VALUES ('04', '张达达', '444', '21', '13564646345');
+INSERT INTO `wbj` (num, name, pwd, age, phone) VALUES ('05', '何水鸥', '555', '18', '15995646345');
+INSERT INTO `wbj` (num, name, pwd, age, phone) VALUES ('06', '贺吉', '666', '22', '15815445855');
 COMMIT;
+
