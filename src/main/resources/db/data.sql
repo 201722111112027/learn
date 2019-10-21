1903162e04a4ecf85e130925bf886a6c8ec1b279
0925sss <1617563206@qq.com>
2019-10-21 16:30:15 +0800

    往表里加数据

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -14,4 +14,10 @@ INSERT INTO `wang` (id, username, password, role) VALUES ('5', 'user3', '1234', 
 INSERT INTO `wang` (id, username, password, role) VALUES ('6', 'user4', '1234', 'user');
 INSERT INTO `wang` (id, username, password, role) VALUES ('7', '测试', '1234', 'user');
 
+INSERT INTO `Leetest` (id, username, password, email, phone) VALUES ('1', 'root', '1234', '11111@qq.com', '12345');
+INSERT INTO `Leetest` (id, username, password, email, phone) VALUES ('2', 'root', '1234', '11111@qq.com', '12345');
+INSERT INTO `Leetest` (id, username, password, email, phone) VALUES ('3', 'root', '1234', '11111@qq.com', '12345');
+INSERT INTO `Leetest` (id, username, password, email, phone) VALUES ('4', 'root', '1234', '11111@qq.com', '12345');
+INSERT INTO `Leetest` (id, username, password, email, phone) VALUES ('5', 'root', '1234', '11111@qq.com', '12345');
+
 COMMIT;
